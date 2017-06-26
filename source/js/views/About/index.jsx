f8a22a664d99a4e347dc5db5f730cc0bfebc30c2
akusany2 <ankitkumar0411@gmail.com>
2017-06-26 16:02:57 +0530

    Added dummy text

--- source/js/views/About/index.jsx
+++ source/js/views/About/index.jsx
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <div className='About'>
+      <div className='container flow-text'>
           <h1>About Movies</h1>
           <p>
               Get shit done with this boilerplate
           </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi ratione doloremque excepturi a in dignissimos dolor optio, reiciendis sunt, quia ipsam ipsa facilis cumque nostrum doloribus vitae perspiciatis quis facere, fugit debitis! Nesciunt, sunt maiores at, illo aut ut doloremque, numquam cumque illum ad incidunt porro quisquam. Tempora praesentium enim, molestiae assumenda recusandae deleniti. Maiores optio, impedit labore pariatur autem cupiditate ad incidunt tempora? Nesciunt corporis aspernatur consequuntur porro perferendis consectetur explicabo iure eligendi voluptates deserunt amet sed veritatis nobis distinctio dolores provident, saepe enim reprehenderit, quasi. Animi quae, vel libero, minima aspernatur mollitia commodi officia iure ea? Ipsum sunt suscipit inventore ea, nemo culpa saepe eaque possimus minus, id consequatur optio. Vero ad excepturi earum vitae a dolores consequuntur doloremque ipsa accusamus magnam quidem, sunt voluptatum, quasi, debitis consectetur tenetur distinctio! A facilis nulla perferendis in possimus! Aspernatur nisi excepturi laboriosam est, ab porro soluta consequatur veniam quaerat aliquam. Quidem magnam facere repudiandae iusto, suscipit, quisquam amet laborum pariatur, consequuntur cum eos dolore, optio dolores soluta vero perspiciatis labore sint vitae corporis. Quis voluptates velit voluptas nemo magnam reiciendis dolorum nulla, nobis quasi, eligendi accusantium, dicta eius odit consequuntur placeat assumenda harum maxime veritatis dolores minus fugit commodi sapiente laborum totam? Reprehenderit, delectus, quis. At laboriosam voluptatem obcaecati iusto soluta facilis facere, dolores eos, possimus quos nihil asperiores dolore voluptatum vero temporibus, quo sunt similique ea modi amet maiores velit beatae mollitia dignissimos? Deserunt natus perferendis, voluptatibus eaque sunt, labore in sapiente autem alias commodi. Dolor doloremque error, molestias.</p>
       </div>
     );
   }
--- source/js/views/NotFound/index.jsx
+++ source/js/views/NotFound/index.jsx
@@ -3,8 +3,12 @@ import React, { Component } from 'react';
 export default class NotFound extends Component {
   render() {
     return (
-      <div className='NotFound'>
-        <h1>Not Found</h1>
+      <div className='container'>
+          <h1>Not Found</h1>
+          <div className="flow-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime cum quidem repellendus voluptate rerum sapiente dignissimos quisquam eum, quod voluptatibus itaque aspernatur magnam, numquam deleniti reprehenderit, consequatur dolorem iste ratione labore culpa, corporis autem atque. Aliquam nulla in debitis accusantium consequatur! Dignissimos vitae consequatur quo hic commodi laboriosam eligendi adipisci. Quam magni minima modi pariatur recusandae sint sapiente aspernatur deserunt doloribus accusamus quod itaque, quidem libero optio velit dolores expedita harum omnis nesciunt quo error voluptate esse. Tempora porro, animi exercitationem facere nulla odit fuga, maxime voluptate quae quidem laudantium commodi omnis sint expedita. Voluptas placeat debitis odio ipsum. Explicabo.
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore porro, quia adipisci velit dignissimos iure deleniti quod culpa in expedita ipsam aut, quisquam debitis voluptas eligendi accusantium. Minus pariatur distinctio, eveniet aperiam at ipsam aspernatur laudantium omnis cum! Repellendus reiciendis, omnis, eaque neque inventore praesentium excepturi nemo, mollitia error perspiciatis aperiam voluptates officiis consequatur velit porro! Enim voluptatem voluptates fugiat rem debitis explicabo optio incidunt magni inventore, reprehenderit autem labore, esse aperiam magnam minus quam quod, ullam exercitationem. Vero quis doloremque aspernatur molestias eveniet possimus, facere ex maiores rem totam nostrum numquam voluptatem labore! Dolor ad, magnam rem consequatur quaerat.</p>
+          </div>
       </div>
     );
   }
